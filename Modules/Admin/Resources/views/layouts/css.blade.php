eef3d82cb3081220af0be724639a7a7b50ad3512
Quyen Le <quyen.lbv@gmail.com>
2017-08-11 09:33:31 +0700

    Quyen

--- Modules/Admin/Resources/views/layouts/css.blade.php
+++ Modules/Admin/Resources/views/layouts/css.blade.php
@@ -1,18 +1,18 @@
 <!-- Bootstrap -->
-<link href="{{module_asset_url('admin')}}/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="{{qasset('public/vendors')}}/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <!-- Font Awesome -->
-<link href="{{module_asset_url('admin')}}/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="{{qasset('public/vendors')}}/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 <!-- NProgress -->
-<link href="{{module_asset_url('admin')}}/vendors/nprogress/nprogress.css" rel="stylesheet">
+<link href="{{qasset('public/vendors')}}/nprogress/nprogress.css" rel="stylesheet">
 <!-- iCheck -->
-<link href="{{module_asset_url('admin')}}/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+<link href="{{qasset('public/vendors')}}/iCheck/skins/flat/green.css" rel="stylesheet">
 
 <!-- bootstrap-progressbar -->
-<link href="{{module_asset_url('admin')}}/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+<link href="{{qasset('public/vendors')}}/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
 <!-- JQVMap -->
-<link href="{{module_asset_url('admin')}}/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+<link href="{{qasset('public/vendors')}}/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
 <!-- bootstrap-daterangepicker -->
-<link href="{{module_asset_url('admin')}}/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+<link href="{{qasset('public/vendors')}}/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
 <!-- Custom Theme Style -->
 <link href="{{module_asset_url('admin')}}/build/css/custom.min.css" rel="stylesheet">
--- Modules/Admin/Resources/views/layouts/js.blade.php
+++ Modules/Admin/Resources/views/layouts/js.blade.php
@@ -1,40 +1,40 @@
 <!-- jQuery -->
-<script src="{{module_asset_url('admin')}}/vendors/jquery/dist/jquery.min.js"></script>
+<script src="{{qasset('public/vendors')}}/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="{{module_asset_url('admin')}}/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{{qasset('public/vendors')}}/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- FastClick -->
-<script src="{{module_asset_url('admin')}}/vendors/fastclick/lib/fastclick.js"></script>
+<script src="{{qasset('public/vendors')}}/fastclick/lib/fastclick.js"></script>
 <!-- NProgress -->
-<script src="{{module_asset_url('admin')}}/vendors/nprogress/nprogress.js"></script>
+<script src="{{qasset('public/vendors')}}/nprogress/nprogress.js"></script>
 <!-- Chart.js -->
-<script src="{{module_asset_url('admin')}}/vendors/Chart.js/dist/Chart.min.js"></script>
+<script src="{{qasset('public/vendors')}}/Chart.js/dist/Chart.min.js"></script>
 <!-- gauge.js -->
-<script src="{{module_asset_url('admin')}}/vendors/gauge.js/dist/gauge.min.js"></script>
+<script src="{{qasset('public/vendors')}}/gauge.js/dist/gauge.min.js"></script>
 <!-- bootstrap-progressbar -->
-<script src="{{module_asset_url('admin')}}/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<script src="{{qasset('public/vendors')}}/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
 <!-- iCheck -->
-<script src="{{module_asset_url('admin')}}/vendors/iCheck/icheck.min.js"></script>
+<script src="{{qasset('public/vendors')}}/iCheck/icheck.min.js"></script>
 <!-- Skycons -->
-<script src="{{module_asset_url('admin')}}/vendors/skycons/skycons.js"></script>
+<script src="{{qasset('public/vendors')}}/skycons/skycons.js"></script>
 <!-- Flot -->
-<script src="{{module_asset_url('admin')}}/vendors/Flot/jquery.flot.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/Flot/jquery.flot.pie.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/Flot/jquery.flot.time.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/Flot/jquery.flot.stack.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/Flot/jquery.flot.resize.js"></script>
+<script src="{{qasset('public/vendors')}}/Flot/jquery.flot.js"></script>
+<script src="{{qasset('public/vendors')}}/Flot/jquery.flot.pie.js"></script>
+<script src="{{qasset('public/vendors')}}/Flot/jquery.flot.time.js"></script>
+<script src="{{qasset('public/vendors')}}/Flot/jquery.flot.stack.js"></script>
+<script src="{{qasset('public/vendors')}}/Flot/jquery.flot.resize.js"></script>
 <!-- Flot plugins -->
-<script src="{{module_asset_url('admin')}}/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/flot.curvedlines/curvedLines.js"></script>
+<script src="{{qasset('public/vendors')}}/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="{{qasset('public/vendors')}}/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="{{qasset('public/vendors')}}/flot.curvedlines/curvedLines.js"></script>
 <!-- DateJS -->
-<script src="{{module_asset_url('admin')}}/vendors/DateJS/build/date.js"></script>
+<script src="{{qasset('public/vendors')}}/DateJS/build/date.js"></script>
 <!-- JQVMap -->
-<script src="{{module_asset_url('admin')}}/vendors/jqvmap/dist/jquery.vmap.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<script src="{{qasset('public/vendors')}}/jqvmap/dist/jquery.vmap.js"></script>
+<script src="{{qasset('public/vendors')}}/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="{{qasset('public/vendors')}}/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
 <!-- bootstrap-daterangepicker -->
-<script src="{{module_asset_url('admin')}}/vendors/moment/min/moment.min.js"></script>
-<script src="{{module_asset_url('admin')}}/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="{{qasset('public/vendors')}}/moment/min/moment.min.js"></script>
+<script src="{{qasset('public/vendors')}}/bootstrap-daterangepicker/daterangepicker.js"></script>
 
 <!-- Custom Theme Scripts -->
 <script src="{{module_asset_url('admin')}}/build/js/custom.js"></script>
